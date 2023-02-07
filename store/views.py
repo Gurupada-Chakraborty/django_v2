7f4b05c9853bab6193535ff2dedb61641dd57138
Gurupada Chakraborty <gurupadachakraborty@Gurupadas-MacBook-Air.local>
2023-02-08 02:49:30 +0530

    model serializers

--- store/views.py
+++ store/views.py
@@ -2,22 +2,41 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import Product, Collection
 from .serializer import ProductSerializer, CollectionSerializer
 
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
-    product = Product.objects.get(pk=id)
-    json_dict = ProductSerializer(product)
-    return Response(json_dict.data)
+    product = get_object_or_404(Product, pk=id)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+    if request.method == 'DELETE':
+        if product.orderitem_set.count() > 0:
+            return Response({'info' : 'Failed, The selected record has other associated fields'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response({'info' : 'Record deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    
 
 @api_view()
 def collection_detail(request, pk):
